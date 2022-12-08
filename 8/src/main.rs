39425ae0a9a6f8c0733e136a593dbaf8e1e3699e
Max Verbeek <m4xv3rb33k@gmail.com>
2022-12-09 00:36:11 +0100

    feat(day 8): pt 2

--- 8/src/main.rs
+++ 8/src/main.rs
@@ -1,23 +1,36 @@
 use std::io::{stdin, BufRead};
 
 struct Tree {
-    height: i32,
+    height: usize,
     visible: bool,
+    scenic: i32,
 }
 
 impl From<char> for Tree {
     fn from(c: char) -> Self {
         Self {
-            height: c.to_digit(10).unwrap() as i32,
+            height: c.to_digit(10).unwrap() as usize,
             visible: false,
+            scenic: 1,
         }
     }
 }
 
-fn topcheck(curtop: &mut i32, tree: &mut Tree) {
-    if tree.height > *curtop {
+fn topcheck(tops: &mut [i32], tree: &mut Tree, location: usize) {
+    // find the scenic multiplier by finding out the location of the tree with the same height
+    // in previous iterations
+    let intersect = tops[tree.height];
+
+    if intersect == -1 {
         tree.visible = true;
-        *curtop = tree.height;
+        tree.scenic *= location as i32;
+    } else {
+        tree.scenic *= (location as i32) - (intersect as i32);
+    }
+
+    // any tree that is not higher than this one will look at this one
+    for h in 0..=tree.height {
+        tops[h] = location as i32;
     }
 }
 
@@ -34,46 +47,53 @@ fn main() {
     // for all rows:
     for y in 0..h {
         // east to west
-        let mut top = -1;
+        let mut tops = [-1; 10];
         for x in 0..w {
-            topcheck(&mut top, &mut lines[y][x]);
+            topcheck(&mut tops, &mut lines[y][x], x);
         }
 
         // west to east
-        let mut top = -1;
+        let mut tops = [-1; 10];
         for x in (0..w).rev() {
-            topcheck(&mut top, &mut lines[y][x]);
+            topcheck(&mut tops, &mut lines[y][x], w - x - 1);
         }
     }
 
     // for all cols
     for x in 0..w {
         // north to south
-        let mut top = -1;
+        let mut tops = [-1; 10];
         for y in 0..h {
-            topcheck(&mut top, &mut lines[y][x]);
+            topcheck(&mut tops, &mut lines[y][x], y);
         }
 
         // south to north
-        let mut top = -1;
+        let mut tops = [-1; 10];
         for y in (0..h).rev() {
-            topcheck(&mut top, &mut lines[y][x]);
+            topcheck(&mut tops, &mut lines[y][x], h - y - 1);
         }
     }
 
     let mut count = 0;
 
-    for line in lines {
+    for line in lines.iter() {
         for tree in line {
             if tree.visible {
                 count += 1;
-                print!("X");
             } else {
-                print!(".");
             }
+            print!("{:01$} ", tree.scenic, 2);
         }
         print!("\n");
     }
 
     println!("visible trees: {}", count);
+
+    let scenic = lines
+        .iter()
+        .map(|l| l.iter().map(|t| t.scenic).max().unwrap())
+        .max()
+        .unwrap();
+
+    println!("highest scenic score: {}", scenic);
 }
