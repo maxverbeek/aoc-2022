288c346adaab73a20fa18f144826d30db2935db4
Max Verbeek <m4xv3rb33k@gmail.com>
2022-12-14 12:54:46 +0100

    feat(day 14): add day 14

--- 14/src/main.rs
+++ 14/src/main.rs
@@ -1,7 +1,9 @@
 use std::{
-    collections::{hash_map::Entry, BTreeMap, BTreeSet, HashMap},
+    collections::{hash_map::Entry, BTreeMap, HashMap},
     error::Error,
+    fmt::Display,
     io::{stdin, BufRead},
+    ops::RangeInclusive,
     str::FromStr,
 };
 
@@ -10,9 +12,27 @@ enum Structure {
     Rock,
 }
 
+#[derive(PartialEq)]
 struct Point {
     x: i32,
     y: i32,
+}
+
+impl Point {
+    fn x_range(&self, other: &Point) -> RangeInclusive<i32> {
+        self.x.min(other.x)..=(self.x.max(other.x))
+    }
+
+    fn y_range(&self, other: &Point) -> RangeInclusive<i32> {
+        self.y.min(other.y)..=(self.y.max(other.y))
+    }
+}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.x, self.y)?;
+        Ok(())
+    }
 }
 
 impl FromStr for Point {
@@ -28,8 +48,25 @@ impl FromStr for Point {
     }
 }
 
+fn draw_map(sandmap: &HashMap<i32, BTreeMap<i32, Structure>>, minx: i32, maxx: i32, maxy: i32) {
+    for y in 0..=maxy {
+        for x in minx..=maxx {
+            match sandmap.get(&x).map(|c| c.get(&y)).flatten() {
+                None => print!("."),
+                Some(Structure::Rock) => print!("#"),
+                Some(Structure::Sand) => print!("o"),
+            }
+        }
+
+        print!("\n");
+    }
+}
+
 fn main() {
     let lines = stdin().lock().lines().map(|l| l.unwrap());
+    let mut minx: i32 = 500;
+    let mut maxx: i32 = 500;
+    let mut maxy: i32 = 0;
 
     // is this a very expensive datastructure for inputs this small?
     let mut sandmap: HashMap<i32, BTreeMap<i32, Structure>> = HashMap::new();
@@ -39,8 +76,12 @@ fn main() {
         let mut from = points.next().expect("must have first point");
 
         for to in points {
-            for x in from.x..=to.x {
-                let rocks = (from.y..=to.y).map(|y| (y, Structure::Rock));
+            minx = minx.min(from.x).min(to.x);
+            maxx = maxx.max(from.x).max(to.x);
+            maxy = maxy.max(from.y).max(to.y);
+
+            for x in from.x_range(&to) {
+                let rocks = from.y_range(&to).map(|y| (y, Structure::Rock));
 
                 match sandmap.entry(x) {
                     // If something already exists on this x, add additional rocks to it
@@ -60,9 +101,26 @@ fn main() {
         }
     }
 
-    let sand = simulate(&mut sandmap);
+    let sand = simulate(&mut sandmap, minx, maxx, maxy);
 
-    println!("total sand: {}", sand); // 0 :DDD
+    println!("total sand with void: {}", sand);
+
+    // add a floor
+    for x in (minx - maxy - 1)..=(maxx + maxy + 1) {
+        match sandmap.entry(x) {
+            Entry::Occupied(mut m) => {
+                m.get_mut().insert(maxy + 2, Structure::Rock);
+            }
+            Entry::Vacant(m) => {
+                m.insert([(maxy + 2, Structure::Rock)].into());
+            }
+        }
+    }
+
+    // Continue the simulation with a capped simulate function
+    let moresand = simulate(&mut sandmap, minx, maxx, maxy);
+
+    println!("total sand with floor: {}", sand + moresand);
 }
 
 fn get_sand_insert_loc(
@@ -71,13 +129,17 @@ fn get_sand_insert_loc(
 ) -> Option<Point> {
     let (&midy, _) = sandmap.get(&current.x)?.range(current.y..).next()?;
 
+    // println!("midy: {}", midy);
+
     // if we are here, the middle column is blocked at (x, midy)
 
-    let (&lefty, _) = sandmap.get(&(current.x - 1))?.range((midy + 1)..).next()?;
+    let (&lefty, _) = sandmap.get(&(current.x - 1))?.range(midy..).next()?;
+
+    // println!("lefty: {}", lefty);
 
     // looking down in the col to the left, the first y we encounter is not a place that occupies a
     // valid sand position
-    if lefty > midy + 1 {
+    if lefty > midy {
         return get_sand_insert_loc(
             sandmap,
             &Point {
@@ -88,7 +150,9 @@ fn get_sand_insert_loc(
     }
 
     // if we are here, the left column is also blocked
-    let (&righty, _) = sandmap.get(&(current.x + 1))?.range((midy + 1)..).next()?;
+    let (&righty, _) = sandmap.get(&(current.x + 1))?.range(midy..).next()?;
+
+    // println!("righty: {}", lefty);
 
     if righty > midy {
         return get_sand_insert_loc(
@@ -107,16 +171,27 @@ fn get_sand_insert_loc(
     })
 }
 
-fn simulate(sandmap: &mut HashMap<i32, BTreeMap<i32, Structure>>) -> usize {
+fn simulate(
+    sandmap: &mut HashMap<i32, BTreeMap<i32, Structure>>,
+    minx: i32,
+    maxx: i32,
+    maxy: i32,
+) -> usize {
     let sandloc = Point { x: 500, y: 0 };
     let mut numsand: usize = 0;
 
     loop {
         match get_sand_insert_loc(sandmap, &sandloc) {
             Some(point) => {
+                // println!("inserting sand on {}", point);
                 let col = sandmap.get_mut(&point.x).unwrap();
                 col.insert(point.y, Structure::Sand);
                 numsand += 1;
+
+                if point == (Point { x: 500, y: 0 }) {
+                    draw_map(sandmap, minx - maxy - 2, maxx + maxy + 2, maxy + 3);
+                    return numsand;
+                }
             }
             None => {
                 return numsand;
