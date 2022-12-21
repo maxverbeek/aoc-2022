9b8e55459cf44fc10bc7cc5b7df9b0800187e04b
Max Verbeek <m4xv3rb33k@gmail.com>
2022-12-21 13:16:20 +0100

    feat: day 21 part 2

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     str::FromStr,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Expr {
     Num(i64),
     Add(String, String),
@@ -21,6 +21,16 @@ impl Expr {
             Expr::Sub(a, b) => (&a, &b),
             Expr::Mul(a, b) => (&a, &b),
             Expr::Div(a, b) => (&a, &b),
+        }
+    }
+
+    fn op(&self) -> &'static str {
+        match self {
+            Expr::Num(_) => "id",
+            Expr::Add(_, _) => "+",
+            Expr::Sub(_, _) => "-",
+            Expr::Mul(_, _) => "*",
+            Expr::Div(_, _) => "/",
         }
     }
 }
@@ -59,6 +69,103 @@ fn eval(monkeys: &HashMap<String, Expr>, key: &str) -> i64 {
     }
 }
 
+fn find_human(monkeys: &HashMap<String, Expr>, key: &str) -> LinearFunc {
+    if key == "humn" {
+        return LinearFunc {
+            slope: 1.0,
+            intercept: 0.0,
+        };
+    }
+
+    let expr = monkeys.get(key).unwrap();
+
+    if let Expr::Num(n) = expr {
+        return LinearFunc {
+            slope: 0.0,
+            intercept: *n as f64,
+        };
+    } else {
+        let (k1, k2) = expr.pairs();
+
+        let mut e1 = find_human(monkeys, k1);
+
+        if e1.slope != 0.0 {
+            // e1 has human component
+            let rhs = eval(monkeys, k2);
+
+            match expr {
+                Expr::Num(_) => unreachable!(),
+                Expr::Add(_, _) => e1.add(rhs),
+                Expr::Sub(_, _) => e1.sub(rhs),
+                Expr::Mul(_, _) => e1.scale(rhs),
+                Expr::Div(_, _) => e1.div(rhs),
+            }
+
+            return e1;
+        }
+
+        let mut e2 = find_human(monkeys, k2);
+
+        if e2.slope != 0.0 {
+            // e2 has human component
+            let lhs = eval(monkeys, k1);
+
+            match expr {
+                Expr::Num(_) => unreachable!(),
+                Expr::Add(_, _) => e2.add(lhs),
+                Expr::Sub(_, _) => {
+                    // lhs - x => -(-lhs + x) => -(x - lhs)
+                    e2.sub(lhs);
+                    e2.scale(-1);
+                }
+                Expr::Mul(_, _) => e2.scale(lhs),
+                Expr::Div(_, _) => unimplemented!("dividing by something with a slope is hard"),
+            }
+
+            return e2;
+        }
+
+        LinearFunc {
+            slope: 0f64,
+            intercept: eval(monkeys, key) as f64,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct LinearFunc {
+    slope: f64,
+    intercept: f64,
+}
+
+impl LinearFunc {
+    fn add(&mut self, x: i64) {
+        self.intercept += x as f64;
+    }
+
+    fn scale(&mut self, x: i64) {
+        self.intercept *= x as f64;
+        self.slope *= x as f64;
+    }
+
+    fn sub(&mut self, x: i64) {
+        self.intercept -= x as f64;
+    }
+
+    fn div(&mut self, x: i64) {
+        self.intercept /= x as f64;
+        self.slope /= x as f64;
+    }
+
+    fn solve(&self, rhs: &Self) -> f64 {
+        if self.slope != 0.0 {
+            (rhs.intercept - self.intercept) / self.slope
+        } else {
+            (self.intercept - rhs.intercept) / rhs.slope
+        }
+    }
+}
+
 fn main() {
     let mut monkeys = HashMap::new();
 
@@ -74,4 +181,13 @@ fn main() {
     let res = eval(&monkeys, "root");
 
     println!("result: {}", res);
+
+    let (k1, k2) = &monkeys.get("root").unwrap().pairs();
+
+    let lhs = find_human(&monkeys, k1);
+    let rhs = find_human(&monkeys, k2);
+
+    let ans = lhs.solve(&rhs) as i64;
+
+    println!("answer for human: {}", ans);
 }
